eec021852f11451bf561e32aa18f9d6ebf37b0cc
JJaviMS <javi.mateosegura@gmail.com>
2023-01-20 13:22:58 +0100

    Fix error on NurApiUsbAutoConnect that disconnect the RFID reader when another USB device which was not the reader was disconnected

--- NurApiAndroid/src/main/java/com/nordicid/nurapi/NurApiUsbAutoConnect.java
+++ NurApiAndroid/src/main/java/com/nordicid/nurapi/NurApiUsbAutoConnect.java
@@ -28,230 +28,251 @@ import android.hardware.usb.UsbManager;
 import android.os.Handler;
 import android.util.Log;
 
-public class NurApiUsbAutoConnect implements NurApiAutoConnectTransport
-{
-	static final String TAG = "NurApiUsbAutoConnect";
+public class NurApiUsbAutoConnect implements NurApiAutoConnectTransport {
+    static final String TAG = "NurApiUsbAutoConnect";
 
-	private NurApi mApi = null;
-	private Context mContext = null;
-	private UsbManager mUsbManager = null;
-	private UsbDevice mUsbDevice = null;
-	private static final String ACTION_USB_PERMISSION = "com.nordicid.nurapi.USB_PERMISSION";
-	private boolean mReceiverRegistered = false;
-	private PendingIntent mPermissionIntent;
-	private boolean mRequestingPermission = false;
+    /**
+     * USB vendor ID for Nordic devices
+     */
+    private static final int NUR_VENDOR_ID_1 = 3589;
 
-	private String mReceiverRegisteredAction = "";
+    /**
+     * USB vendor ID for Nordic devices
+     */
+    private static final int NUR_VENDOR_ID_2 = 1254;
 
-	private boolean mEnabled = false;
+    /**
+     * USB product ID for Nordic RFID devices
+     */
+    private static final int NUR_PRODUCT_ID_1 = 274;
 
-	void registerReceiver(String action)
-	{
-		if (action.length() == 0)
-		{
-			if (mReceiverRegistered)
-			{
-				mReceiverRegistered = false;
-				mReceiverRegisteredAction = "";
-				mContext.unregisterReceiver(mUsbReceiver);
-				Log.d(TAG, "registerReceiver unregistered");
-			} else {
-				Log.d(TAG, "registerReceiver ALREADY unregistered");
-			}
-		}
-		else {
+    /**
+     * USB product ID for Nordic RFID devices
+     */
+    private static final int NUR_PRODUCT_ID_2 = 2321;
 
-			if (mReceiverRegisteredAction.equals(action))
-			{
-				Log.d(TAG, "registerReceiver "+action+" ALREADY registered");
-				return;
-			}
+    private NurApi mApi = null;
+    private Context mContext = null;
+    private UsbManager mUsbManager = null;
+    private UsbDevice mUsbDevice = null;
+    private static final String ACTION_USB_PERMISSION = "com.nordicid.nurapi.USB_PERMISSION";
+    private boolean mReceiverRegistered = false;
+    private PendingIntent mPermissionIntent;
+    private boolean mRequestingPermission = false;
 
-			if (mReceiverRegistered)
-				mContext.unregisterReceiver(mUsbReceiver);
+    private String mReceiverRegisteredAction = "";
 
-			IntentFilter intentFilter = new IntentFilter();
-			intentFilter.addAction(ACTION_USB_PERMISSION);
-			intentFilter.addAction(action);
-			Log.d(TAG, "registerReceiver "+action+" registered");
-			mContext.registerReceiver(mUsbReceiver, intentFilter);
-			mReceiverRegistered = true;
-			mReceiverRegisteredAction = action;
-		}
-	}
+    private boolean mEnabled = false;
 
-	public NurApiUsbAutoConnect(Context c, NurApi na) 
-	{
-		this.mContext = c;
-		this.mApi = na;
-		this.mUsbManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
-		
-	    mPermissionIntent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), 0);
-	}
+    void registerReceiver(String action) {
+        if (action.length() == 0) {
+            if (mReceiverRegistered) {
+                mReceiverRegistered = false;
+                mReceiverRegisteredAction = "";
+                mContext.unregisterReceiver(mUsbReceiver);
+                Log.d(TAG, "registerReceiver unregistered");
+            } else {
+                Log.d(TAG, "registerReceiver ALREADY unregistered");
+            }
+        } else {
 
-	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() 
-	{
-		@Override
-		public void onReceive(Context context, Intent intent) 
-		{
-			String action = intent.getAction();
-			if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action))
-			{
-				synchronized (this) {
-					Log.d(TAG, "ACTION_USB_DEVICE_ATTACHED " + intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false));
-					mUsbDevice = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
-					{
-						if (mUsbDevice != null) {
-							NurApiUsbAutoConnect.this.setAddress(getAddress());
-						}
-					} 
-					else {
-						Log.d(TAG, "ACTION_USB_DEVICE_ATTACHED permission denied for device " + mUsbDevice);
-						Log.d(TAG, "FORCE CONNECTION to" + getAddress());
-						NurApiUsbAutoConnect.this.setAddress(getAddress());
-					}
-				}
-			} 
-			else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-				Log.d(TAG, "ACTION_USB_DEVICE_DETACHED");
-				disconnect();
-			}
-			else if (ACTION_USB_PERMISSION.equals(action))
-			{
-				mRequestingPermission = false;
-				Log.d(TAG, "ACTION_USB_PERMISSION " + intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false));
+            if (mReceiverRegisteredAction.equals(action)) {
+                Log.d(TAG, "registerReceiver " + action + " ALREADY registered");
+                return;
+            }
 
-				if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
-					NurApiUsbAutoConnect.this.setAddress(getAddress());
-			}
-		}
-	};
+            if (mReceiverRegistered)
+                mContext.unregisterReceiver(mUsbReceiver);
 
-	private void connect()
-	{
-		if (mUsbDevice != null && mUsbManager.hasPermission(mUsbDevice)) 
-		{
-			try {
-				mApi.setTransport(null);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			NurApiUsbTransport tr = new NurApiUsbTransport(mUsbManager, mUsbDevice);
-			try {
-				mApi.setTransport(tr);
-				mApi.connect();
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(ACTION_USB_PERMISSION);
+            intentFilter.addAction(action);
+            Log.d(TAG, "registerReceiver " + action + " registered");
+            mContext.registerReceiver(mUsbReceiver, intentFilter);
+            mReceiverRegistered = true;
+            mReceiverRegisteredAction = action;
+        }
+    }
 
-				registerReceiver(UsbManager.ACTION_USB_DEVICE_DETACHED);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-		else if(mUsbDevice != null && !mUsbManager.hasPermission(mUsbDevice))
-		{
-			mRequestingPermission = true;
-			mUsbManager.requestPermission(mUsbDevice,mPermissionIntent);
-		}
-	}
+    public NurApiUsbAutoConnect(Context c, NurApi na) {
+        this.mContext = c;
+        this.mApi = na;
+        this.mUsbManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
 
-	private void disconnect() 
-	{
-		if (mApi.isConnected()) {
-			try {
-				registerReceiver(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-				mApi.disconnect();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Override
-	public void onResume() 
-	{
-		if (mApi.isConnected())
-			return;
+        mPermissionIntent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), 0);
+    }
 
-		if (!mRequestingPermission)
-			this.setAddress(getAddress());
-	}
-	
-	@Override
-	public void onDestroy() 
-	{
-		
-	}
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+                synchronized (this) {
+                    Log.d(TAG, "ACTION_USB_DEVICE_ATTACHED " + intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false));
+                    UsbDevice intentUsbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    // If the usb device is not a Nordic one do nothing
+                    if (!checkIsNurDevice(intentUsbDevice)){
+                        return;
+                    }
+                    mUsbDevice = intentUsbDevice;
 
-	@Override
-	public void setAddress(String addr)
-	{
-		if (addr == null)
-			addr = "";
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (mUsbDevice != null) {
+                            NurApiUsbAutoConnect.this.setAddress(getAddress());
+                        }
+                    } else {
+                        Log.d(TAG, "ACTION_USB_DEVICE_ATTACHED permission denied for device " + mUsbDevice);
+                        Log.d(TAG, "FORCE CONNECTION to" + getAddress());
+                        NurApiUsbAutoConnect.this.setAddress(getAddress());
+                    }
+                }
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                UsbDevice intentUsbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                // Check the disconnected device is the one that we have connected and no other USB device
+                // We check that the vendor ID matches with one of the Nur IDs and the product ID also matches
+                if (checkIsNurDevice(intentUsbDevice)) {
+                    Log.d(TAG, "ACTION_USB_DEVICE_DETACHED");
+                    disconnect();
+                }
 
-		Log.d(TAG, "setAddress " + addr);
+            } else if (ACTION_USB_PERMISSION.equals(action)) {
+                mRequestingPermission = false;
+                Log.d(TAG, "ACTION_USB_PERMISSION " + intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false));
 
-		mEnabled = addr.length() > 0;
+                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
+                    NurApiUsbAutoConnect.this.setAddress(getAddress());
+            }
+        }
+    };
 
-		this.mUsbDevice = null;
-		for (UsbDevice device : mUsbManager.getDeviceList().values()) {
-			if(device.getVendorId() == 1254 || device.getVendorId() == 3589) {
-				this.mUsbDevice = device;
-				break;
-			}
-		}
+    /**
+     * Checks if the given usbDevice is a valid NUR device
+     * @param usbDevice Usb device to check if is NUR or not
+     * @return True if the usb device is a Nur device, false otherwise
+     */
+    private static boolean checkIsNurDevice(UsbDevice usbDevice) {
+        return usbDevice != null && (usbDevice.getVendorId() == NUR_VENDOR_ID_1 || usbDevice.getVendorId() == NUR_VENDOR_ID_2)
+                && (usbDevice.getProductId() == NUR_PRODUCT_ID_1 || usbDevice.getProductId() == NUR_PRODUCT_ID_2);
+    }
 
-		if (mUsbDevice == null) {
-			registerReceiver(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-		} else {
-			registerReceiver(UsbManager.ACTION_USB_DEVICE_DETACHED);
-		}
+    private void connect() {
+        if (mUsbDevice != null && mUsbManager.hasPermission(mUsbDevice)) {
+            try {
+                mApi.setTransport(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            NurApiUsbTransport tr = new NurApiUsbTransport(mUsbManager, mUsbDevice);
+            try {
+                mApi.setTransport(tr);
+                mApi.connect();
 
-		if (mUsbDevice != null) {
-			new Handler().postDelayed(new Runnable() {
-				@Override
-				public void run() {
-					connect();
-				}
-			}, 200);
-		}
-	}
+                registerReceiver(UsbManager.ACTION_USB_DEVICE_DETACHED);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else if (mUsbDevice != null && !mUsbManager.hasPermission(mUsbDevice)) {
+            mRequestingPermission = true;
+            mUsbManager.requestPermission(mUsbDevice, mPermissionIntent);
+        }
+    }
 
-	@Override
-	public void onPause() {
-	}
+    private void disconnect() {
+        if (mApi.isConnected()) {
+            try {
+                registerReceiver(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+                mApi.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void onStop() {
-		if (mApi.isConnected()) {
-			try {
-				mApi.disconnect(); 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		registerReceiver("");
-	}
+    @Override
+    public void onResume() {
+        if (mApi.isConnected())
+            return;
 
-	@Override
-	public void dispose() {
-		onStop();
-	}
+        if (!mRequestingPermission)
+            this.setAddress(getAddress());
+    }
 
-	@Override
-	public String getType() {
-		return "USB";
-	}
+    @Override
+    public void onDestroy() {
 
-	@Override
-	public String getAddress() { return mEnabled ? "USB" : ""; }
+    }
 
-	@Override
-	public String getDetails() {
-		if (mApi.isConnected())
-			return "Connected to USB";
-		else if (!mEnabled)
-			return "Disabled";
+    @Override
+    public void setAddress(String addr) {
+        if (addr == null)
+            addr = "";
 
-		return "Disconnected from USB";
-	}
+        Log.d(TAG, "setAddress " + addr);
+
+        mEnabled = addr.length() > 0;
+
+        this.mUsbDevice = null;
+        for (UsbDevice device : mUsbManager.getDeviceList().values()) {
+            if (device.getVendorId() == 1254 || device.getVendorId() == 3589) {
+                this.mUsbDevice = device;
+                break;
+            }
+        }
+
+        if (mUsbDevice == null) {
+            registerReceiver(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        } else {
+            registerReceiver(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        }
+
+        if (mUsbDevice != null) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    connect();
+                }
+            }, 200);
+        }
+    }
+
+    @Override
+    public void onPause() {
+    }
+
+    @Override
+    public void onStop() {
+        if (mApi.isConnected()) {
+            try {
+                mApi.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        registerReceiver("");
+    }
+
+    @Override
+    public void dispose() {
+        onStop();
+    }
+
+    @Override
+    public String getType() {
+        return "USB";
+    }
+
+    @Override
+    public String getAddress() {
+        return mEnabled ? "USB" : "";
+    }
+
+    @Override
+    public String getDetails() {
+        if (mApi.isConnected())
+            return "Connected to USB";
+        else if (!mEnabled)
+            return "Disabled";
+
+        return "Disconnected from USB";
+    }
 }
